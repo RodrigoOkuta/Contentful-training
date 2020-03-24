4c254d5e40dd2c0e4282f1f59068f11384fa6df1
Rodrigo Issao Okuta Watanabe <rodrigookuta@Rodrigos-MacBook-Pro.local>
2020-03-23 21:39:04 -0400

    Added banner to the pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 const Home = () => {
-    return (
-        <Hero>
+  return (
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn-primary">our rooms</Link>
+      </Banner>
+    </Hero>
+  );
+};
 
-        </Hero>
-    )
-}
-
-export default Home
+export default Home;
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 const Rooms = () => {
-    return (
-        <Hero hero="roomsHero">
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
 
-        </Hero>
-    )
-}
-
-export default Rooms
+export default Rooms;
