47f9260db10206512f606254111ca493c5bc4240
Rodrigo Issao Okuta Watanabe <rodrigookuta@Rodrigos-MacBook-Pro.local>
2020-03-24 20:06:30 -0400

    Implemented Services component

---
+++ src/components/Services.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+
+import Title from "./Title";
+
+const Services = () => {
+  const [services] = useState([
+    {
+      icon: <FaCocktail />,
+      title: "Free Cocktails",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, velit!"
+    },
+    {
+      icon: <FaHiking />,
+      title: "Endless Hiking",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, velit!"
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: "Free Shuttle",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, velit!"
+    },
+    {
+      icon: <FaBeer />,
+      title: "Strongest Beer",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, velit!"
+    }
+  ]);
+
+  return (
+    <section className="services">
+      <Title title="services" />
+      <div className="services-center">
+        {services.map((item, index) => {
+          return (
+            <article key={index} className="service">
+              <span>{item.icon}</span>
+              <h6>{item.title}</h6>
+              <p>{item.info}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
