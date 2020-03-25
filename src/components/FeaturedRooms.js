ef81f95f9c44f25083cfd57186dbd2ac7d8bc18e
Rodrigo <rodrigo.okuta@gmnail.com>
2020-03-25 19:47:07 -0400

    Implemented featuredRooms and Room component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { useStateValue } from "../context";
+
 import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
+import { useStateValue } from "../context";
 
 const FeaturedRooms = () => {
-  const [{ featuredRooms }, dispatch] = useStateValue();
+  const [{ featuredRooms: rooms, loading }, dispatch] = useStateValue();
 
-  return <div>hello from featured rooms</div>;
+  if (loading) return <Loading />;
+
+  const getRooms = () => rooms.map(room => <Room key={room.id} room={room} />);
+
+  return (
+    <section className="featured-rooms">
+      <Title title="featured rooms" />
+      <div className="featured-rooms-center">{getRooms()}</div>
+    </section>
+  );
 };
 
 export default FeaturedRooms;
