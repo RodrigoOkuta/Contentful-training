c29dfd4fa324721c3b429fa07f66af954d47085c
Rodrigo <rodrigo.okuta@gmnail.com>
2020-03-29 13:51:17 -0400

    Created roomFilter component

---
+++ src/components/RoomFilter.js
@@ -0,0 +1,132 @@
+import React from "react";
+
+import { useStateValue } from "../context";
+import Title from "./Title";
+
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+
+const RoomFilter = ({ rooms }) => {
+  const {
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+    handleChange
+  } = useStateValue();
+
+  const types = ["all", ...getUnique(rooms, "type")];
+
+  const people = getUnique(rooms, "capacity");
+
+  const renderSelectType = () => {
+    return types.map((type, index) => (
+      <option key={index} value={type}>
+        {type}
+      </option>
+    ));
+  };
+  const renderSelectCapacity = () => {
+    return people.map((person, index) => (
+      <option key={index} value={person}>
+        {person}
+      </option>
+    ));
+  };
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            className="form-control"
+            value={type}
+            onChange={handleChange}
+          >
+            {renderSelectType()}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="capacity">Guests</label>
+          <select
+            name="capacity"
+            id="capacity"
+            className="form-control"
+            value={capacity}
+            onChange={handleChange}
+          >
+            {renderSelectCapacity()}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">room price ${price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="size">room size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">pets</label>
+          </div>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default RoomFilter;
