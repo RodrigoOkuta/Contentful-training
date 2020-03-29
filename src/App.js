e95720fd59defd57eb377b453f4b9a4d9154fbb7
Rodrigo <rodrigo.okuta@gmnail.com>
2020-03-29 13:49:53 -0400

    change app.ja to class component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import { StateProvider } from "./context";
-import items from "./data";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
@@ -10,23 +9,43 @@ import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 import "./App.css";
 import Loading from "./components/Loading";
+import items from "./data";
 
-const App = () => {
-  const [rooms, setRooms] = useState([]);
-  const [sortedRooms, setSortedRooms] = useState([]);
-  const [featuredRooms, setFeaturedRooms] = useState([]);
-  const [loading, setLoading] = useState(true);
+class App extends Component {
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
+  };
 
-  useEffect(() => {
-    let rooms = formatData(items);
-    setRooms(rooms);
-    setSortedRooms(rooms);
-    let featuredRooms = rooms.filter(room => room.featured);
-    setFeaturedRooms(featuredRooms);
-    setLoading(false);
-  }, []);
+  componentDidMount() {
+    const rooms = this.formatData(items);
+    const featuredRooms = rooms.filter(room => room.featured);
+    const maxPrice = Math.max(...rooms.map(room => room.price));
+    const maxSize = Math.max(...rooms.map(room => room.size));
 
-  const formatData = items => {
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
+    });
+  }
+
+  formatData = items => {
     let tempItems = items.map(item => {
       let id = item.sys.id;
       let images = item.fields.images.map(image => image.fields.file.url);
@@ -38,39 +57,78 @@ const App = () => {
     return tempItems;
   };
 
-  const getRoomBySlug = slug => {
+  getRoomBySlug = slug => {
+    const { rooms } = this.state;
     return rooms.find(room => room.slug === slug);
   };
 
-  const reducer = (state, action) => {
-    switch (action.type) {
-      default:
-        return state;
-    }
+  handleChange = event => {
+    const {
+      target: { checked, value, name, type }
+    } = event;
+    const changeValue = type === "checkbox" ? checked : value;
+
+    this.setState({ [name]: changeValue }, this.filterRooms);
   };
 
-  if (loading) return <Loading />;
+  filterRooms = () => {
+    const {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
 
-  return (
-    <StateProvider
-      initialState={{
-        rooms,
-        sortedRooms,
-        featuredRooms,
-        loading,
-        getRoomBySlug: getRoomBySlug
-      }}
-      reducer={reducer}
-    >
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms" component={Rooms} />
-        <Route exact path="/rooms/:slug" component={SingleRoom} />
-        <Route component={Error} />
-      </Switch>
-    </StateProvider>
-  );
-};
+    let tempRooms = [...rooms];
+    if (type !== "all")
+      tempRooms = tempRooms.filter(room => room.type === type);
+
+    const selectedCapacity = parseInt(capacity);
+
+    if (selectedCapacity !== 1)
+      tempRooms = tempRooms.filter(room => room.capacity >= selectedCapacity);
+
+    const selectedPrice = parseInt(price);
+    if (selectedPrice > 0)
+      tempRooms = tempRooms.filter(room => room.price <= selectedPrice);
+
+    tempRooms = tempRooms.filter(
+      room => room.size >= parseInt(minSize) && room.size <= parseInt(maxSize)
+    );
+
+    breakfast &&
+      (tempRooms = tempRooms.filter(room => room.breakfast === breakfast));
+
+    pets && (tempRooms = tempRooms.filter(room => room.pets === pets));
+
+    this.setState({ sortedRooms: tempRooms });
+  };
+
+  render() {
+    const { loading } = this.state;
+    if (loading) return <Loading />;
+
+    const helpers = {
+      getRoomBySlug: this.getRoomBySlug,
+      handleChange: this.handleChange
+    };
+
+    return (
+      <StateProvider initialState={{ ...this.state, ...helpers }}>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/rooms" component={Rooms} />
+          <Route exact path="/rooms/:slug" component={SingleRoom} />
+          <Route component={Error} />
+        </Switch>
+      </StateProvider>
+    );
+  }
+}
 
 export default App;
